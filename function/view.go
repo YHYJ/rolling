214eb2c8aaf2bd8d18115ce70b72ab031c780d34
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-09-21 16:25:27 +0800

    E: run_command.go —— RunCommandGetResult函数新增返回值error

--- function/view.go
+++ function/view.go
@@ -20,34 +20,40 @@ func SystemInfo() {
 	var fileName = "/var/log/pacman.log"
 
 	// 获取系统安装时间和当前时间
-	var lineText = ReadFileLine(fileName, 1)
-	var startTimeStrTZ = strings.Split(strings.Split(lineText, "[")[1], "]")[0] // 2023-03-10T10:49:09+0800
-	var currentTimeStr = time.Now().Format("2006-01-02 15:04")
+	lineText := ReadFileLine(fileName, 1)
+	startTimeStrTZ := strings.Split(strings.Split(lineText, "[")[1], "]")[0] // 2023-03-10T10:49:09+0800
+	currentTimeStr := time.Now().Format("2006-01-02 15:04")
 
 	// 获取初始和当前内核版本
-	var keyText = ReadFileKey(fileName, "installed linux ")
-	var firstKernel = strings.Split(strings.Split(keyText, " (")[1], ")")[0]
-	var unameArgs = []string{"-r"}
-	var latestKernel = RunCommandGetResult("uname", unameArgs)
+	keyText := ReadFileKey(fileName, "installed linux ")
+	firstKernel := strings.Split(strings.Split(keyText, " (")[1], ")")[0]
+	unameArgs := []string{"-r"}
+	latestKernel, err := RunCommandGetResult("uname", unameArgs)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m\n", err)
+	}
 
 	// 计算系统安装天数
-	var local, _ = time.LoadLocation("Asia/Shanghai")
-	var startTime, _ = time.ParseInLocation("2006-01-02T15:04:05Z0700", startTimeStrTZ, local)
-	var startTimeStr = startTime.Format("2006-01-02 15:04")
-	var startTimeStamp = startTime.Unix()
-	var currentTime, _ = time.ParseInLocation("2006-01-02 15:04", currentTimeStr, local)
-	var currentTimeStamp = currentTime.Unix()
-	var systemDays = (currentTimeStamp - startTimeStamp) / 86400
+	local, _ := time.LoadLocation("Asia/Shanghai")
+	startTime, _ := time.ParseInLocation("2006-01-02T15:04:05Z0700", startTimeStrTZ, local)
+	startTimeStr := startTime.Format("2006-01-02 15:04")
+	startTimeStamp := startTime.Unix()
+	currentTime, _ := time.ParseInLocation("2006-01-02 15:04", currentTimeStr, local)
+	currentTimeStamp := currentTime.Unix()
+	systemDays := (currentTimeStamp - startTimeStamp) / 86400
 
 	// 获取系统/内核更新相关数据
-	var systemUpdateCount = ReadFileCount(fileName, "system_checkupdate.hook")
-	var systemUpdateMean = float32(systemUpdateCount) / float32(systemDays)
-	var kernelUpdateCount = ReadFileCount(fileName, "upgraded linux ")
-	var kernelUpdateMean = float32(systemDays) / float32(kernelUpdateCount)
+	systemUpdateCount := ReadFileCount(fileName, "system_checkupdate.hook")
+	systemUpdateMean := float32(systemUpdateCount) / float32(systemDays)
+	kernelUpdateCount := ReadFileCount(fileName, "upgraded linux ")
+	kernelUpdateMean := float32(systemDays) / float32(kernelUpdateCount)
 
 	// 获取吉祥物
-	var repoArgs = []string{""}
-	var mascot = RunCommandGetResult("repo-elephant", repoArgs)
+	repoArgs := []string{""}
+	mascot, err := RunCommandGetResult("repo-elephant", repoArgs)
+	if err != nil {
+		fmt.Printf("\x1b[31m%s\x1b[0m\n", err)
+	}
 
 	// 输出
 	fmt.Printf("\033[36m[%16v]\033[0m %-2v \033[36m[%-16v]\033[0m\n", startTimeStr, "--", currentTimeStr)
