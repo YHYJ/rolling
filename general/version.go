ac38e276627c3b1998f4a74772c607700a604870
YJ-ArchLinux-Master <yj1516268@outlook.com>
2024-04-02 22:09:44 +0800

    v0.5.8

--- general/version.go
+++ general/version.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	Name    string = "Rolling"                 // 程序名
-	Version string = "v0.5.7"                  // 程序版本
+	Version string = "v0.5.8"                  // 程序版本
 	Project string = "github.com/yhyj/rolling" // 项目地址
 )
 
