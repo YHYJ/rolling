c77bd9fa8a66122722c5ceaae8d92fd05100474c
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-04-08 15:54:06 +0800

    v0.6.4

--- general/version.go
+++ general/version.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	Name    string = "Rolling"                 // 程序名
-	Version string = "v0.6.3"                  // 程序版本
+	Version string = "v0.6.4"                  // 程序版本
 	Project string = "github.com/yhyj/rolling" // 项目地址
 )
 
